b0df1577b8d5e8b109308d4567c888647b394339
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-09-24 12:21:20 +0200

    🍒 new country each day

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,71 @@
-import { GuessGridContainer } from "../components/GuessGridContainer";
-import { Nav } from "../components/Nav";
-import React, { FormEvent, useRef } from "react";
-import { useCapitalGameStore } from "../stores/capitalGameStore";
-import { HintDetails } from "../components/HintDetails";
+import { GetStaticProps } from 'next'
+import { GuessGridContainer } from '../Components/GuessGridContainer'
+import { Nav } from '../Components/Nav'
+import React, { FormEvent, useRef } from 'react'
+import { useCapitalGameStore } from '../stores/capitalGameStore'
+import { HintDetails } from '../Components/HintDetails'
 
-import { capitals } from "../constants/capitals";
-import { countries } from "countries-list";
+import { capitals } from '../constants/capitals'
+import { countries, Country } from 'countries-list'
+import { getTodaysCountry } from '../todays-utils/useTodays'
 
-const MAX_GUESSES = 6;
+const MAX_GUESSES = 6
 
-export default function Web() {
-  const country = useCapitalGameStore(({ country }) => country);
-  const hintCount = useCapitalGameStore(({ hintCount }) => hintCount);
-  const incrementHintCount = useCapitalGameStore(
-    ({ incrementHintCount }) => incrementHintCount
-  );
-  const setGameStateSlices = useCapitalGameStore(
-    ({ setGameStateSlices }) => setGameStateSlices
-  );
-  const guesses = useCapitalGameStore(({ guesses }) => guesses);
-  const setGuesses = useCapitalGameStore(({ setGuesses }) => setGuesses);
-  // const gameStatus = useCapitalGameStore(({ gameStatus }) => gameStatus);
-  // const gameStateSlices = useCapitalGameStore(
-  //   ({ gameStateSlices }) => gameStateSlices
-  // );
+export default function CapitalsGame() {
+    const country = useCapitalGameStore(({ country }) => country)
+    const hintCount = useCapitalGameStore(({ hintCount }) => hintCount)
+    const incrementHintCount = useCapitalGameStore(
+        ({ incrementHintCount }) => incrementHintCount
+    )
+    const setGameStateSlices = useCapitalGameStore(
+        ({ setGameStateSlices }) => setGameStateSlices
+    )
+    const guesses = useCapitalGameStore(({ guesses }) => guesses)
+    const setGuesses = useCapitalGameStore(({ setGuesses }) => setGuesses)
+    // const gameStatus = useCapitalGameStore(({ gameStatus }) => gameStatus);
+    // const gameStateSlices = useCapitalGameStore(
+    //   ({ gameStateSlices }) => gameStateSlices
+    // );
 
-  const guessCount = guesses.length;
-  const hasGuessesRemaining = MAX_GUESSES > guessCount;
-  const isCorrect = guesses.includes(country.capital.toLocaleLowerCase());
-  const isLoser = !isCorrect && guessCount === MAX_GUESSES;
-  const gameOver = !hasGuessesRemaining || isCorrect || isLoser;
+    const guessCount = guesses.length
+    const hasGuessesRemaining = MAX_GUESSES > guessCount
+    const isCorrect = guesses.includes(country.capital.toLocaleLowerCase())
+    const isLoser = !isCorrect && guessCount === MAX_GUESSES
+    const gameOver = !hasGuessesRemaining || isCorrect || isLoser
 
-  const hasHintsRemaining = country.capital.length <= hintCount;
+    const hasHintsRemaining = country.capital.length <= hintCount
 
-  const inputRef = useRef<HTMLInputElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null)
 
-  const handleGuessSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setGuesses(inputRef.current?.value ?? "");
-    setGameStateSlices();
-  };
+    const handleGuessSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setGuesses(inputRef.current?.value ?? '')
+        setGameStateSlices()
+    }
 
-  const handleGuessClick = () => {
-    setGuesses(inputRef.current?.value ?? "");
-    setGameStateSlices();
-  };
+    const handleGuessClick = () => {
+        setGuesses(inputRef.current?.value ?? '')
+        setGameStateSlices()
+    }
 
-  return (
-    <div className="w-screen grid grid-cols-7 overflow-hidden">
-      <div className="col-start-2 col-span-5 md:col-start-3 md:col-span-3">
-        <Nav />
+    return (
         <div className="">
-          <h2 className="text-3xl my-4 text-center font-bold text-gray-700 uppercase">
-            {country.name}
-          </h2>
-          <h3 className="text-3xl text-center my-4">{country.emoji}</h3>
-        </div>
+            <div className="">
+                <Nav />
+                <div className="">
+                    <h2 className="text-3xl my-4 text-center font-bold text-gray-700 uppercase">
+                        {country.name}
+                    </h2>
+                    <h3 className="text-3xl text-center my-4">
+                        {country.emoji}
+                    </h3>
+                </div>
 
-        <HintDetails />
-        <GuessGridContainer />
+                <HintDetails />
+                <GuessGridContainer />
 
-        {/* <form className="w-ful" onSubmit={handleGuessSubmit}> */}
-        {/* <Select
+                {/* <form className="w-ful" onSubmit={handleGuessSubmit}> */}
+                {/* <Select
           ref={inputRef}
           className="w-full appearance rounded
         text-gray-700 my-1 leading-tight
@@ -72,26 +76,24 @@ export default function Web() {
           data={[...capitals]}
           disabled={gameOver}
         /> */}
-        {/* </form> */}
-        <button
-          className="w-full rounded py-1 px-6 border-2
+                {/* </form> */}
+                <button
+                    className="w-full rounded py-1 px-6 border-2
         hover:bg-slate-50
         disabled:bg-slate-300 disabled:cursor-not-allowed"
-          onClick={handleGuessClick}
-          disabled={gameOver}
-        >
-          Guess
-        </button>
-        <button
-          className="w-full rounded py-1 px-6 my-2 border-2 
+                    onClick={handleGuessClick}
+                    disabled={gameOver}>
+                    Guess
+                </button>
+                <button
+                    className="w-full rounded py-1 px-6 my-2 border-2 
         hover:bg-slate-50
         disabled:bg-slate-300 disabled:cursor-not-allowed"
-          onClick={incrementHintCount}
-          disabled={gameOver || hasHintsRemaining}
-        >
-          Hint
-        </button>
-      </div>
-    </div>
-  );
+                    onClick={incrementHintCount}
+                    disabled={gameOver || hasHintsRemaining}>
+                    Hint
+                </button>
+            </div>
+        </div>
+    )
 }
--- stores/capitalGameStore.tsx
+++ stores/capitalGameStore.tsx
@@ -1,71 +1,74 @@
-import { countries, Country } from "countries-list";
-import create from "zustand";
+import { Country } from 'countries-list'
+import create from 'zustand'
+import { getTodaysCountry } from '../todays-utils/useTodays'
 
 export enum GameStatus {
-  PLAYING = "PLAYING",
-  COMPLETE = "COMPLETE",
-  SUCCESS = "SUCCESS",
-  FAIL = "FAIL",
+    PLAYING = 'PLAYING',
+    COMPLETE = 'COMPLETE',
+    SUCCESS = 'SUCCESS',
+    FAIL = 'FAIL',
 }
 
-export type GameStatusType = keyof typeof GameStatus;
+export type GameStatusType = keyof typeof GameStatus
 export type GameStateSliceType = {
-  guesses: string[];
-  guess: string;
-  hintCount: number;
-  isCorrect: boolean;
-};
+    guesses: string[]
+    guess: string
+    hintCount: number
+    isCorrect: boolean
+}
 
 export type CapitalGameStore = {
-  gameStatus: GameStatusType;
-  country: Country;
-  guesses: string[];
-  hintCount: number;
-  gameStateSlices: GameStateSliceType[];
-  incrementHintCount: () => void;
-  setGameStateSlices: () => void;
-  setGameStatus: (gameStatus: GameStatusType) => void;
-  setGuesses: (guess: string) => void;
-};
+    gameStatus: GameStatusType
+    country: Country
+    guesses: string[]
+    hintCount: number
+    gameStateSlices: GameStateSliceType[]
+    setCountry: (country: Country) => void
+    incrementHintCount: () => void
+    setGameStateSlices: () => void
+    setGameStatus: (gameStatus: GameStatusType) => void
+    setGuesses: (guess: string) => void
+}
 
-export const useCapitalGameStore = create<CapitalGameStore>((set) => ({
-  country: countries.CO,
-  gameStatus: GameStatus.PLAYING,
-  guesses: [],
-  hintCount: 0,
-  gameStateSlices: [],
-  incrementHintCount: () =>
-    set((state) => ({ ...state, hintCount: state.hintCount + 1 })),
-  setGameStateSlices: () =>
-    set((state) => {
-      const currentGuess =
-        [...state.guesses].pop()?.toLocaleLowerCase().trim() || "";
-      const isCorrect =
-        currentGuess === state.country.capital.toLocaleLowerCase();
-      return {
-        ...state,
-        gameStateSlices: [
-          ...state.gameStateSlices,
-          {
-            guesses: state.guesses,
-            hintCount: state.hintCount,
-            isCorrect,
-            guess: currentGuess,
-          },
-        ],
-      };
-    }),
-  setHintCount: () =>
-    set((state) => ({ ...state, hintCount: state.hintCount + 1 })),
+export const useCapitalGameStore = create<CapitalGameStore>(set => ({
+    country: getTodaysCountry(),
+    gameStatus: GameStatus.PLAYING,
+    guesses: [],
+    hintCount: 0,
+    gameStateSlices: [],
+    setCountry: country => set(state => ({ ...state, country })),
+    incrementHintCount: () =>
+        set(state => ({ ...state, hintCount: state.hintCount + 1 })),
+    setGameStateSlices: () =>
+        set(state => {
+            const currentGuess =
+                [...state.guesses].pop()?.toLocaleLowerCase().trim() || ''
+            const isCorrect =
+                currentGuess === state?.country.capital.toLocaleLowerCase()
+            return {
+                ...state,
+                gameStateSlices: [
+                    ...state.gameStateSlices,
+                    {
+                        guesses: state.guesses,
+                        hintCount: state.hintCount,
+                        isCorrect,
+                        guess: currentGuess,
+                    },
+                ],
+            }
+        }),
+    setHintCount: () =>
+        set(state => ({ ...state, hintCount: state.hintCount + 1 })),
 
-  setGameStatus: (gameStatus: GameStatusType) =>
-    set((state) => ({
-      ...state,
-      gameStatus,
-    })),
-  setGuesses: (guess) =>
-    set((state) => ({
-      ...state,
-      guesses: [...state.guesses, guess.toLocaleLowerCase().trim()],
-    })),
-}));
+    setGameStatus: (gameStatus: GameStatusType) =>
+        set(state => ({
+            ...state,
+            gameStatus,
+        })),
+    setGuesses: guess =>
+        set(state => ({
+            ...state,
+            guesses: [...state.guesses, guess.toLocaleLowerCase().trim()],
+        })),
+}))
