e75705004323e79ec0cbfe47956382f85a118f54
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2022-11-06 21:34:24 +0100

    darker

--- components/CapitalsLogo.tsx
+++ components/CapitalsLogo.tsx
@@ -12,6 +12,6 @@ export const CapitalsLogo = ({ size }: CapitalsLogoProps) => (
             size === CapitalsLogoSize.LARGE ? 'text-4xl' : 'text-1xl'
         }  uppercase`}>
         Capita
-        <span className="text-blue-500">l</span>s
+        <span className="text-blue-500 dark:text-pink-800">l</span>s
     </h1>
 )
