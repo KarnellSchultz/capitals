f15a34b2c32fb4e11e07a17a50d7d864050f5a0f
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-01-12 15:30:46 +0100

    chnage runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { CapitalsGame } from './client-page'
 
-export const runtime = 'experimental-edge'; // 'node.js' (default) | 'experimental-edge'
-
 export default function page() {
     return <CapitalsGame />
 }
