8ae2975e5d1c58f3536c178e465e2c4955d28410
Karnell Schultz <50870173+KarnellSchultz@users.noreply.github.com>
2023-01-12 15:22:46 +0100

    remove console log

--- components/HintDetails.tsx
+++ components/HintDetails.tsx
@@ -8,7 +8,6 @@ type HintDetailsProps = {
     hintCount: number
 }
 export const HintDetails = ({ capital, hintCount }: HintDetailsProps) => {
-    console.log("🚀 ~ file: HintDetails.tsx:11 ~ HintDetails ~ capital", capital)
     const [parent] = useAutoAnimate()
 
     const hintedWord = capital.substring(0, hintCount)
